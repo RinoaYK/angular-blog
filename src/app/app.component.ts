a41a56fb47c2050f32c1f65158cd166c7beb95f0
RinoaYK <rinoayk@gmail.com>
2025-03-09 20:14:59 -0300

    Componente Menu, Rotas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-//import { RouterOutlet } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
+import { RouterOutlet } from '@angular/router';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HomeComponent],
+  imports: [RouterOutlet, MenuBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
